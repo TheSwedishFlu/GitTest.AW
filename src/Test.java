edba51ed5bc4cd0b2aa96e9c1c496bd9adc1d4aa
UlfHéden <uffeheden@gmail.com>
2022-08-22 13:33:59 +0200

    Deleted some

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("change");
-        System.out.println("again");
+
     }
 }
