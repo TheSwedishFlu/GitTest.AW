00476b7a635d0bffbddcfd8c7fb90969d73e0c45
UlfHéden <uffeheden@gmail.com>
2022-08-22 15:30:39 +0200

    Udate from master

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("This is a cloned repo");
+        System.out.println("This is updated on master");
 
     }
 }
